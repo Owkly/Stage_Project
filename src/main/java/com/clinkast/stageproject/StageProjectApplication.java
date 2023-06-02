fc16dda6d5d667f45de83b14b100b7e4b91269d7
weikai <yannickzhang23@gmail.com>
2023-06-02 16:59:50 +0200

    Initial commit

---
+++ src/main/java/com/clinkast/stageproject/StageProjectApplication.java
@@ -0,0 +1,13 @@
+package com.clinkast.stageproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StageProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StageProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinkast/stageproject/StageProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinkast.stageproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StageProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
