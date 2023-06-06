6b0eeca64243e00d6614747a0b11819344179e3d
yannick <yannickzhang23@gmail.om>
2023-06-06 13:37:39 +0200

    Affichage de la BD - Yannick

--- src/main/java/com/clinkast/stageproject/DataConnection.java
+++ src/main/java/com/clinkast/stageproject/DataConnection.java
@@ -1,8 +1,7 @@
 package com.clinkast.stageproject;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+// Connetion DriverManager Statement ResulSet SQLException
 
 public class DataConnection {
     public static void main(String[] args) {
@@ -12,9 +11,26 @@ public class DataConnection {
 
         try (Connection conn = DriverManager.getConnection(url, user, password)) {
             System.out.println("Connected to the database");
+
+            String sql = "SELECT * FROM stop";
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("Id");
+                String stopName = resultSet.getString("stopName");
+                String previousStop = resultSet.getString("previousStop");
+                String nextStop = resultSet.getString("nextStop");
+                System.out.println("Id : " + id);
+                System.out.println("Stop Name : " + stopName);
+                System.out.println("Previous Stop : " + previousStop);
+                System.out.println("Next Stop : " + nextStop);
+                System.out.println("-----------------------------");
+            }
         } catch (SQLException e) {
             System.out.println("Failed to connect to the database");
             e.printStackTrace();
         }
     }
 }
+
