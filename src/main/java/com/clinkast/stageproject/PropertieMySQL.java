036928588cf9d500186cfef9d2b19198e0563fad
yannick <yannickzhang23@gmail.om>
2023-06-07 16:47:35 +0200

    flyway - Yannick

--- src/main/java/com/clinkast/stageproject/PropertieMySQL.java
+++ src/main/java/com/clinkast/stageproject/PropertieMySQL.java
@@ -4,6 +4,7 @@ public class PropertieMySQL {
     // Initialisation des variables statics pour la connection a MySql
     public static String url = "jdbc:mysql://localhost:3306/metro";
     public static String user = "root";
-    // public static String password = "stage"; // mdp Yannick
-    public static String password = "0000"; // mdp Steeve
+    public static String password = "stage"; // mdp Yannick
+
+    //public static String password = "0000"; // mdp Steeve
 }
---
+++ src/main/resources/db/migration/V1__Create_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE stop (
+  Id INT PRIMARY KEY,
+  stopName VARCHAR(255),
+  previousStop VARCHAR(255),
+  nextStop VARCHAR(255)
+);
