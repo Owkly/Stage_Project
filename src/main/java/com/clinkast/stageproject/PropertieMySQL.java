508b6c075fcf0ee4ca830582e39718d9981fce23
yannick <yannickzhang23@gmail.om>
2023-06-07 09:30:13 +0200

    Affichage de la BD - Yannick

---
+++ src/main/java/com/clinkast/stageproject/PropertieMySQL.java
@@ -0,0 +1,7 @@
+package com.clinkast.stageproject;
+
+public class PropertieMySQL {
+    public String url = "jdbc:mysql://localhost:3306/metro";
+    public String user = "root";
+    public String password = "stage";
+}
