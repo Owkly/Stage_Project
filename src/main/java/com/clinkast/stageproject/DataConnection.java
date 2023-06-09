d55ac93067f34b3b7a7105ae235ae4f6164a50d2
Steeve <steeve.xu@outlook.fr>
2023-06-09 10:46:33 +0200

    Vendredi 09 Juin - Présentation html de la BDD

--- src/main/java/com/clinkast/stageproject/DataConnection.java
+++ src/main/java/com/clinkast/stageproject/DataConnection.java
@@ -19,25 +19,57 @@ public class DataConnection {
 
             // Objet htmlContent
             StringBuilder htmlContent = new StringBuilder();
-            htmlContent.append("<html><body><table>");
+            htmlContent.append("<!DOCTYPE html>\n<html>\n<head>\n");
 
             // Header row
-            htmlContent.append("<tr><th>Id</th><th>Stop Name</th><th>Previous Stop</th><th>Next Stop</th></tr>");
+            htmlContent.append("<title>Présentation des arrêts</title>\n" +
+                    "  <meta charset=\"UTF-8\">\n" +
+                    "  <style>\n" +
+                    "   table {\n" +
+                    "       border-collapse: collapse;\n" +
+                    "       width: 100%;\n" +
+                    "   }\n" +
+                    "   th, td {\n" +
+                    "       border: 1px solid black;\n" +
+                    "       padding: 8px;\n" +
+                    "       text-align: center;\n" +
+                    "   }\n" +
+                    "   th {\n" +
+                    "       background-color: #f2f2f2;\n" +
+                    "   }\n" +
+                            "   .centered-title {\n" +
+                            "      text-align: center;\n" +
+                            "       color: red;\n" +
+                            "    }\n" +
+                    "   </style>\n" +
+                    "</head>\n"
+                    );
+
+            // Body row
+            htmlContent.append("<body>\n" +
+                    "   <h1 class='centered-title'>Présentation des arrêts</h1>\n" +
+                    "   <table>\n" +
+                    "   <tr>\n" +
+                    "       <th>ID</th>\n" +
+                    "       <th>Arrêt</th>\n" +
+                    "       <th>Arrêt précédent</th>\n" +
+                    "       <th>Arrêt suivant</th>\n" +
+                    "   </tr>\n");
 
             while (resultSet.next()) {
                 int id = resultSet.getInt("Id");
                 String stopName = resultSet.getString("stopName");
                 String previousStop = resultSet.getString("previousStop");
                 String nextStop = resultSet.getString("nextStop");
-                htmlContent.append("<tr>")
-                        .append("<td>").append(id).append("</td>")
-                        .append("<td>").append(stopName).append("</td>")
-                        .append("<td>").append(previousStop != null ? previousStop : "").append("</td>")
-                        .append("<td>").append(nextStop != null ? nextStop : "").append("</td>")
-                        .append("</tr>");
+                htmlContent.append("<tr>\n")
+                        .append("<td>").append(id).append("</td>\n")
+                        .append("<td>").append(stopName).append("</td>\n")
+                        .append("<td>").append(previousStop != null ? previousStop : "").append("</td>\n")
+                        .append("<td>").append(nextStop != null ? nextStop : "").append("</td>\n")
+                        .append("</tr>\n");
             }
 
-            htmlContent.append("</table></body></html>");
+            htmlContent.append("</table>\n</body>\n</html>");
 
             // Write HTML content to file
             FileWriter fileWriter = new FileWriter(HTMLPath.url);
